09bf96871a4c43a167b637a28d9aa32d5a1c233a
zloyleva <zloyleva@gmail.com>
2019-12-30 21:01:59 +0200

    add HW

--- js/app.js
+++ js/app.js
@@ -1,19 +1,31 @@
 console.log('App was loaded...')
 
-// axios
-//     .get('https://zloyleva-imt.github.io/lesson_09/js/data.json')
-fetch('https://zloyleva-imt.github.io/lesson_09/js/data.json')
-    .then(res => res.json())
+axios
+    .get('https://rickandmortyapi.com/api/character/',)
     .then(res => {
-        const { devices } = res
-        // const devices = res.devices;
+        // const { devices } = res
+        // // const devices = res.devices;
 
-        const obj = new Object;
-        devices.map(el => Array.isArray(obj[el.placeRu]) 
-            ? 
-                obj[el.placeRu].push(el) 
-            : 
-                obj[el.placeRu] = [el])
+        // const obj = new Object;
+        // devices.forEach(el => Array.isArray(obj[el.placeRu]) 
+        //     ? 
+        //         obj[el.placeRu].push(el) 
+        //     : 
+        //         obj[el.placeRu] = [el])
 
-        console.log(obj);
+        // console.log();
+        const {pages} = res.data.info
+
+        // 'https://rickandmortyapi.com/api/character/?page=1';
+
+        // const linksArr = [...new Array(pages)].map((el,i) => `https://rickandmortyapi.com/api/character/?page=${i+1}`)
+        // console.log(linksArr);
+
+        // Promise.all(linksArr.map(el => axios.get(el)))
+        //     .then(res => console.log(res));
+
+        localStorage.setItem('password', 'super strong password')
+
+        Promise.all([...new Array(pages)].map((el,i) => axios.get(`https://rickandmortyapi.com/api/character/?page=${i+1}`)))
+            .then(res => console.log(res));
     })
