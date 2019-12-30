aaa5a0dd0199eb1c83ae75bbae8b3b19c2a597ea
zloyleva <zloyleva@gmail.com>
2019-12-30 19:21:23 +0200

    Group ATM

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
 console.log('App was loaded...')
 
-const axios = require('axios').default;
+// axios
+//     .get('https://zloyleva-imt.github.io/lesson_09/js/data.json')
+fetch('https://zloyleva-imt.github.io/lesson_09/js/data.json')
+    .then(res => res.json())
+    .then(res => {
+        const { devices } = res
+        // const devices = res.devices;
+
+        const obj = new Object;
+        devices.map(el => Array.isArray(obj[el.placeRu]) 
+            ? 
+                obj[el.placeRu].push(el) 
+            : 
+                obj[el.placeRu] = [el])
+
+        console.log(obj);
+    })
